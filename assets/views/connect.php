335306569edeab22f7e66386b862c93f93ffc34d
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-08-16 11:07:11 +0200

    Remove password

--- assets/views/connect.php
+++ assets/views/connect.php
@@ -5,7 +5,7 @@ try {
     $host = 'localhost';
     $dbname = 'id20943308_becode';
     $user = 'id20943308_alexp';
-    $pwd = 'NypenseMemeP@s7';
+    $pwd = '';
     $hash = password_hash($pwd, PASSWORD_DEFAULT);
 
     $bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user , $pwd, [
